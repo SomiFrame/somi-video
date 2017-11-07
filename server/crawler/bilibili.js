aa9c518d19722a9feeb738dbf301fc0714d67212
somi <somi@somis-MacBook-Pro.local>
2017-11-07 22:45:26 +0700

    优化爬虫类‘

--- server/crawler/bilibili.js
+++ server/crawler/bilibili.js
@@ -1,18 +1,28 @@
 var Crawler = require('crawler');
+var domain = "http://www.amazon.com";
 var c = new Crawler({
     maxConnections: 10,
-    callback: (error,res,done)=>{
-        if(error){
+    callback: (error, res, done) => {
+        if (error) {
             console.log(error);
             throw error;
-        }else {
+        } else {
             var $ = res.$;
-            console.log($("title").text());
+            $('a').each((index, a) => {
+                if (a.attribs.href) {
+                    console.log(index, a.attribs.href);
+                    console.log(typeof a.attribs.href);
+                    if (a.attribs.href.toString().match(/\/\//)) {
+                        c.queue(a.attribs.href);
+                    } else {
+                        c.queue(domain + a.attribs.href);
+                    }
+                }
+            });
         }
         done();
     }
 });
 //queue just one url , with default callback
-c.queue('https://www.amazon.com');
-c.queue(['https://www.google.com','https://www.yahoo.com']);
+c.queue(domain);
 
